52fa38bf9077c53ca85a3d80e861f260310e4913
kalehub <teguhsatyadharma@gmail.com>
2020-05-15 19:57:19 +0800

    iam rich

---
+++ iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iamrich
+//
+//  Created by Teguh Satya on 15/05/20.
+//  Copyright © 2020 Teguh Satya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
